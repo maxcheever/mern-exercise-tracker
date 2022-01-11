20b57c6b947a6c0ba4703b148e70f98707e315ae
Max Cheever <maxcheever8@hotmail.com>
2022-01-10 22:46:33 -0500

    changed port

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}...`);
+    console.log(`Server is running on port ${port}`);
 });
